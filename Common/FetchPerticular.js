f056364c4f336e0aea1d8cd32259528a79caad59
velsanjay <sanjaymech2313@mail.com>
2023-05-21 00:22:14 +0530

    error

--- Common/FetchPerticular.js
+++ Common/FetchPerticular.js
@@ -8,7 +8,7 @@ const fetchPerticular = async ({page,show,query}) => {
       let result;
       await axios
         .get(
-          `https://api.themoviedb.org/3/search/${show}?api_key=3ccf0748623e30bf009b38eb3b9c6417&query=${query}&page=${page}&include_adult=true`
+          `https://api.themoviedb.org/3/search/${show}?api_key=3ccf0748623e30bf009b38eb3b9c6417&query=${query}&page=${page}`
         )
         .then((response) => {
           result = response.data.results;
